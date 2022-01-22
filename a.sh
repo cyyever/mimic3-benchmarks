791be15b8134bc737d7a301972008cbf35620cd6
cyy <cyyever@outlook.com>
2022-01-22 22:09:29 +0800

    add script

---
+++ a.sh
@@ -0,0 +1,11 @@
+mkdir -p data
+# python3 -m mimic3benchmark.scripts.extract_subjects ~/mimic3/physionet.org/files/mimiciii/1.4/ data/root
+# python3 -m mimic3benchmark.scripts.validate_events data/root
+# python3 -m mimic3benchmark.scripts.extract_episodes_from_subjects data/root/
+
+python3 -m mimic3benchmark.scripts.create_in_hospital_mortality data/root/ data/in-hospital-mortality/
+python3 -m mimic3benchmark.scripts.create_decompensation data/root/ data/decompensation/
+python3 -m mimic3benchmark.scripts.create_length_of_stay data/root/ data/length-of-stay/
+python3 -m mimic3benchmark.scripts.create_phenotyping data/root/ data/phenotyping/
+python3 -m mimic3benchmark.scripts.create_multitask data/root/ data/multitask/
+# python3 -m mimic3benchmark.scripts.split_train_and_test data/root/
